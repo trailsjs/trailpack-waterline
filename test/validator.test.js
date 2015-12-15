097eae2bc3f451bfcb35e2dbd33a501e9694d01a
Travis Webb <me@traviswebb.com>
2015-12-15 17:02:08 -0500

    [test] test lib.Validator

---
+++ test/validator.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert')
+const app = require('./testapp')
+const lib = require('../lib')
+
+describe('lib.Validator', () => {
+  describe('#validateDatabaseConfig', () => {
+    it('should validate a valid database config', () => {
+      return lib.Validator.validateDatabaseConfig(app.config.database).then(assert)
+    })
+  })
+})
+
